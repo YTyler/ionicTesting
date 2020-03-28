8edee21223df13b37a42b1eb9bf1710afc7d251c
Tyler C. Yates <tyates907@gmail.com>
2020-03-27 17:50:33 -0700

    add lists, avatar & labels

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,38 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonText } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonText, IonAvatar, IonLabel } from '@ionic/react';
 import React from 'react';
 import './Home.css';
+
+const arr = [
+  {
+    name: 'Finn',
+    desc: 'Ex-Imperial Storm Trooper'
+  },
+  {
+    name: 'Han',
+    desc: 'I shot first'
+  },
+  {
+    name: 'Luke',
+    desc: 'Get me out of this desert'
+  }
+]
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Header Toolbar</IonTitle>
+          <IonTitle>Ionic Testing</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonList>
-          {Array(10).fill(NaN).map((_, i) => (
-          <IonItem key={i}>
-            <IonText>List Item {i}</IonText>
+          {arr.map((e) => (
+          <IonItem key={e.name}>
+            <IonAvatar>
+              <img src={`https://ionicframework.com/docs/demos/api/list/avatar-${e.name.toLowerCase()}.png`} alt={e.name} />
+            </IonAvatar>
+            <IonLabel>{e.desc}</IonLabel>
           </IonItem>
           ))}
           
